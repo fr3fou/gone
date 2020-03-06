777ab02416c5e492a73eaaa444181d50cdf09247
Simo Aleksandrov <simo3003@me.com>
2020-03-06 15:21:59 +0200

    init + make a perceptron

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("gosh")
+	fmt.Println("gosh")
 }
---
+++ perceptron.go
@@ -0,0 +1,39 @@
+package main
+
+import "math/rand"
+
+const Inputs = 2
+
+type Perceptron struct {
+	Weights [2]float64
+}
+
+func NewPerceptron() *Perceptron {
+	weights := [Inputs]float64{}
+	for i := 0; i < Inputs; i++ {
+		weights[i] = float64(rand.Intn(2) - 1)
+	}
+
+	return &Perceptron{
+		Weights: weights,
+	}
+}
+
+func (p *Perceptron) Guess(inputs []float64) int {
+	sum := 0.0
+
+	for i := range inputs {
+		sum += inputs[i] * p.Weights[i]
+	}
+
+	return Sign(sum)
+}
+
+// Sign is an activation function
+func Sign(n float64) int {
+	if n >= 0 {
+		return 1
+	}
+
+	return -1
+}
