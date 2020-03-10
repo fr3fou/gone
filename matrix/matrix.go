5aaffcd6f70fbc11e694f1afa27aa618288008f5
Simo Aleksandrov <simo3003@me.com>
2020-03-10 16:39:19 +0200

    refactor Add

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -75,15 +75,9 @@ func AddMatrix(m, n Matrix) Matrix {
 }
 
 func Add(m Matrix, n float64) Matrix {
-	result := New(m.Rows, m.Columns, m.Data)
-
-	for i := 0; i < m.Rows; i++ {
-		for j := 0; j < m.Columns; j++ {
-			result.Data[i][j] += n
-		}
-	}
-
-	return result
+	return Map(m, func(val float64, x, y int) float64 {
+		return val + n
+	})
 }
 
 func (m Matrix) String() string {
