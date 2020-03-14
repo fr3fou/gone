983a8301dd3cbfd06d7adfecc7c7cd6bf95fa6ad
Simo Aleksandrov <simo3003@me.com>
2020-03-14 03:26:49 +0200

    don't have 1 off computation outside the loop

--- gone/gone.go
+++ gone/gone.go
@@ -169,28 +169,20 @@ func (n *NeuralNetwork) backpropagate(ds DataSample) {
 
 	err := targets.SubtractMatrix(outputs)
 
-	gradients :=
-		// We can also do n.Activations[lenActivations-1]
-		// As the output of the NN is the same as the final activations
-		outputs.
-			Map(func(val float64, x, y int) float64 {
-				return n.Layers[lenLayers-1].Activator.FPrime(val)
-			}).
-			HadamardProduct(err).
-			Scale(n.LearningRate)
+	var gradients matrix.Matrix
+	var deltas matrix.Matrix
 
-	deltas := gradients.DotProduct(
-		n.Activations[lenWeights-1].Transpose(),
-	)
-
-	n.Weights[lenWeights-1].AddMatrix(deltas)
-
-	for i := len(n.Weights) - 2; i > 0; i-- {
+	for i := lenWeights - 1; i > 0; i-- {
 		// The next error is equal to the current error multiplied
 		// by the previous weight matrix but transposed!
 		// The outputs of the previous layer must match with the inputs
 		// of the current layer (the current layer's errors have that shape)
-		err = n.Weights[i+1].Transpose().DotProduct(err)
+		//
+		// We need not compute the error the first time as it was done
+		// outside the loop
+		if i < lenWeights-1 {
+			err = n.Weights[i+1].Transpose().DotProduct(err)
+		}
 
 		gradients = n.Activations[i+1].
 			Map(func(val float64, x, y int) float64 {
