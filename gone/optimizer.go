15911c5388bbb40d389b3a69359772105771dc9a
Simo Aleksandrov <simo3003@me.com>
2020-03-31 03:13:41 +0300

    more wip optimizers

--- gone/optimizer.go
+++ gone/optimizer.go
@@ -7,65 +7,61 @@ type Optimizer func(n *NeuralNetwork, dataSet DataSet)
 
 // SGD is Stochastic Gradient Descent (On-line Training)
 func SGD() Optimizer {
+	// OK THIS WORKS
 	return func(n *NeuralNetwork, dataSet DataSet) {
-		dataSet.Shuffle()
-		for _, ds := range dataSet {
-			inputs := matrix.NewFromArray(ds.Inputs)
-			targets := matrix.NewFromArray(ds.Targets)
-			outputs := n.predict(inputs)
-
-			lenLayers := len(n.Layers)
-			lenWeights := lenLayers - 1 // always one less than the layers (we don't have weights for the inputs)
-
-			err := targets.SubtractMatrix(outputs)
-
-			var gradients matrix.Matrix
-			var deltas matrix.Matrix
-
-			for i := lenWeights - 1; i >= 0; i-- {
-				// The next error is equal to the current error multiplied
-				// by the previous weight matrix but transposed!
-				// The outputs of the previous layer must match with the inputs
-				// of the current layer (the current layer's errors have that shape)
-				//
-				// We need not compute the error the first time as it was done
-				// outside the loop
-				if i < lenWeights-1 {
-					err = n.Weights[i+1].Transpose().DotProduct(err)
-				}
-
-				gradients = n.Activations[i+1].
-					Map(func(val float64, x, y int) float64 {
-						return n.Layers[i+1].Activator.FPrime(val)
-					}).
-					HadamardProduct(err).
-					Scale(n.LearningRate)
-
-				deltas = gradients.
-					DotProduct(n.Activations[i].Transpose())
-
-				n.Weights[i] = n.Weights[i].AddMatrix(deltas)
-				n.Biases[i] = n.Biases[i].AddMatrix(gradients)
-			}
-		}
+		MGBD(1)(n, dataSet)
 	}
 }
 
 // MBGD is a Mini-Batch Gradient Descent (Batch training)
 func MGBD(batchSize int) Optimizer {
+	// TODO: fix to work with any batchSize (currently works only with 1 - SGD)
 	return func(n *NeuralNetwork, dataSet DataSet) {
 		for i := 0; i < len(dataSet); i++ {
 			batch := dataSet.Batch(i, batchSize)
 			batch.Shuffle()
-			// for _, ds := range batch {
 
-			// }
+			lenLayers := len(n.Layers)
+			lenWeights := lenLayers - 1
+
+			for _, ds := range batch {
+				inputs := matrix.NewFromArray(ds.Inputs)
+				targets := matrix.NewFromArray(ds.Targets)
+				outputs := n.predict(inputs)
+
+				// TODO: custom loss
+				err := targets.SubtractMatrix(outputs)
+
+				var currentGradients matrix.Matrix
+				var currentDeltas matrix.Matrix
+
+				for i := lenWeights - 1; i >= 0; i-- {
+					// Ignore the first time
+					if i < lenWeights-1 {
+						err = n.Weights[i+1].Transpose().DotProduct(err)
+					}
+
+					currentGradients = n.Activations[i+1].
+						Map(func(val float64, x, y int) float64 {
+							return n.Layers[i+1].Activator.FPrime(val)
+						}).
+						HadamardProduct(err).
+						Scale(n.LearningRate)
+
+					currentDeltas = currentGradients.
+						DotProduct(n.Activations[i].Transpose())
+
+					n.Weights[i] = n.Weights[i].AddMatrix(currentDeltas)
+					n.Biases[i] = n.Biases[i].AddMatrix(currentGradients)
+				}
+			}
 		}
 	}
 }
 
-// GD is a normal gradient descent
+// GD is a normal gradient descent (Optimizes after the entire data set)
 func GD() Optimizer {
+	// TODO: should work after fixing normal MBGD
 	return func(n *NeuralNetwork, dataSet DataSet) {
 		MGBD(len(dataSet))(n, dataSet)
 	}
