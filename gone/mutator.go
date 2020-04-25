5a4a1088f415c0130bd309497756c943c62825fa
Simo Aleksandrov <simo3003@me.com>
2020-04-26 02:04:27 +0300

    support cusotm stdenv and mean

--- gone/mutator.go
+++ gone/mutator.go
@@ -10,10 +10,10 @@ import (
 type Mutator func(val float64) float64
 
 // GaussianMutation applies a randomly distributed gaussian mutation using mutationRate which should be a number in the range [0.0, 1.0] and represents a probability for a mutation to occur
-func GaussianMutation(mutationRate float64) Mutator {
+func GaussianMutation(mutationRate float64, stdenv, mean float64) Mutator {
 	return func(val float64) float64 {
 		if rand.Float(0, 1) >= mutationRate {
-			mutation := stdrand.NormFloat64() * 0.5 // stdev = 0.5
+			mutation := stdrand.NormFloat64()*stdenv + mean // stdev = 0.5
 			return val + mutation
 		}
 
